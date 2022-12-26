b5d1dc91250232b0975cc6dfa3ae9d15cf52cd46
Ilya Strelkovskiy <IStrelkovskiy@alfabank.ru>
2022-12-26 15:37:44 +0300

    fix(*): remove useful classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [TypeOrmModule.forRoot(dataSourceOptions), MeetupModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
