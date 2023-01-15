3a04efa373c4f41c9b9b416e60dbd33314cf3a47
Ilya Strelkovskiy <IStrelkovskiy@alfabank.ru>
2023-01-15 14:51:25 +0300

    feat(type): create general type fo generatedKeywords

---
+++ src/types/generated-keywords.ts
@@ -0,0 +1,3 @@
+import { MeetupEntity } from '../meetup/entity/meetup.entity';
+
+export type GeneratedKeywords = Array<{ name: string; meetup: MeetupEntity }>;
--- src/utils/generate-keywords.ts
+++ src/utils/generate-keywords.ts
@@ -1,6 +1,11 @@
 import { MeetupEntity } from '../meetup/entity/meetup.entity';
 
-export const generateKeywords = (keywords: string[], meetup: MeetupEntity) =>
+import type { GeneratedKeywords } from '../types/generated-keywords';
+
+export const generateKeywords = (
+    keywords: string[],
+    meetup: MeetupEntity,
+): GeneratedKeywords =>
     keywords.map((keyword) => ({
         name: keyword,
         meetup,
